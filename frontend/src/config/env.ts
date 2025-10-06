9cd7f0b83a2c117832f39c4182c089abf0a50f5a
dagem12 <0zerocool99@gmail.com>
2025-10-06 23:29:41 +0300

    fix: config default value chang

--- frontend/src/config/env.ts
+++ frontend/src/config/env.ts
@@ -1,5 +1,5 @@
 export const config = {
-  API_URL: import.meta.env.VITE_API_URL || 'http://196.189.149.214	:3000',
+  API_URL: import.meta.env.VITE_API_URL || 'http://196.189.149.214:3000',
   SOCKET_URL: import.meta.env.VITE_SOCKET_URL || 'http://196.189.149.214:3000',
   APP_NAME: import.meta.env.VITE_APP_NAME || 'Chat App',
   APP_VERSION: import.meta.env.VITE_APP_VERSION || '1.0.0',
